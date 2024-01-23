9b864bd5f33a7a9cd49b646a2053b04e5688ec4e
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-23 23:03:34 +0300

    Ücrete göre verilecek olan sayfa görüntülenme sayıları belirlenmesi işlemi yapıldı.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,13 @@
 const pricingInput = document.querySelector('#pricing-input');
 const price = document.querySelector('#price');
+const pageViews = document.querySelector('.pageviews');
+const billingSelector = document.querySelector('#month-year-selector');
 
 pricingInput.addEventListener('input',(e) => getValue(e));
-
 function getValue(e){
-    price.textContent = `$${e.target.value}.00`
+    let inputValue = e.target.value
+    price.textContent = `$${inputValue}.00`;
+
+    pageViews.textContent = `${inputValue * 5}K PAGEVIEWS`;
+
 }
