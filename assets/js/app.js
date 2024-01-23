7e39e363c3e430f28c470eac19374894ddf0f24b
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-23 23:52:38 +0300

    Hover işlemleri tamamlandı.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ const pricingInput = document.querySelector('#pricing-input');
 const price = document.querySelector('#price');
 const pageViews = document.querySelector('.pageviews');
 const billingSelector = document.querySelector('#month-year-selector');
+const toggleFill = document.querySelector('.toggle-fill');
 
 pricingInput.addEventListener('input',(e) => getValue(e));
 
@@ -23,11 +24,13 @@ billingSelector.addEventListener('click',billingType);
 function billingType(){
     if(this.classList.contains('active')){
         this.classList.remove('active');
+        toggleFill.style.background = "#CFD8EF";
         inputValue = pricingInput.value;
         price.textContent = `$${inputValue}.00`;
         pageViews.textContent = `${inputValue * 5}K PAGEVIEWS`;
     }else{
         this.classList.add('active');
+        toggleFill.style.background = "#7AEADF";
         inputValue = pricingInput.value * 12;
         price.textContent = `$${inputValue - inputValue/4}.00`;
         pageViews.textContent = `${inputValue * 5}K PAGEVIEWS`;
