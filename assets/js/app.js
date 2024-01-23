7fc3a1c9289f4d1b2e7effb9fd4335a9510998b8
Barış Kahraman <baris-kahra@hotmail.com>
2024-01-23 23:42:55 +0300

    Monthly ve Yearly arasında geçiş yaparken anlık oalrak inputtan alınan değer ile ücretin ve sayfa görüntülenmelerinin sayısı değiştirilmesi işlemi yapıldı. JS tamamlandı.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,32 @@ const pageViews = document.querySelector('.pageviews');
 const billingSelector = document.querySelector('#month-year-selector');
 
 pricingInput.addEventListener('input',(e) => getValue(e));
+
 function getValue(e){
     let inputValue = e.target.value
-    price.textContent = `$${inputValue}.00`;
+    if(billingSelector.classList.contains('active')){
+        inputValue = inputValue * 12;
+        price.textContent = `$${inputValue - inputValue/4}.00`;
+        pageViews.textContent = `${(inputValue - inputValue/4) * 5}K PAGEVIEWS`;
+    }else{
+        price.textContent = `$${inputValue}.00`;
+        pageViews.textContent = `${inputValue * 5}K PAGEVIEWS`;
+    }
+    
+}
 
-    pageViews.textContent = `${inputValue * 5}K PAGEVIEWS`;
+billingSelector.addEventListener('click',billingType);
 
+function billingType(){
+    if(this.classList.contains('active')){
+        this.classList.remove('active');
+        inputValue = pricingInput.value;
+        price.textContent = `$${inputValue}.00`;
+        pageViews.textContent = `${inputValue * 5}K PAGEVIEWS`;
+    }else{
+        this.classList.add('active');
+        inputValue = pricingInput.value * 12;
+        price.textContent = `$${inputValue - inputValue/4}.00`;
+        pageViews.textContent = `${(inputValue - inputValue/4) * 5}K PAGEVIEWS`;
+    }
 }
